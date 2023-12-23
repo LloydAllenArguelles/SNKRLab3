c2f924fa3ca296719f02a60fc26cbfbe66dc1f64
LloydAllenArguelles <allen.arguelles27@gmail.com>
2023-12-23 21:25:50 +0800

    Initial commit

---
+++ app/src/main/java/com/techbuddy/snkrlab3/MainActivity2.java
@@ -0,0 +1,40 @@
+package com.techbuddy.snkrlab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class MainActivity2 extends AppCompatActivity {
+    Button Prcd_button;
+    ImageButton nike_button;
+    ImageButton placeholder_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        nike_button = findViewById(R.id.nike_button);
+        nike_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+        placeholder_button = findViewById(R.id.placeholder_button);
+        placeholder_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
