5265709116ea1f8250c362ef144c96b0a9cc7d66
LloydAllenArguelles <allen.arguelles27@gmail.com>
2023-12-24 01:52:56 +0800

    its working bruh

--- app/src/main/java/com/techbuddy/snkrlab3/MainActivity2.java
+++ app/src/main/java/com/techbuddy/snkrlab3/MainActivity2.java
@@ -32,7 +32,7 @@ public class MainActivity2 extends AppCompatActivity {
         placeholder_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                Intent intent = new Intent(MainActivity2.this, CameraActivity.class);
                 startActivity(intent);
             }
         });
