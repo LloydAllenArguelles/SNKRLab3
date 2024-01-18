a5136b832088a17bf2ebe469fa943d2937853160
LloydAllenArguelles <allen.arguelles27@gmail.com>
2024-01-18 20:31:52 +0800

    1

--- app/src/main/java/com/techbuddy/snkrlab3/AdidasActivity.java
+++ app/src/main/java/com/techbuddy/snkrlab3/AdidasActivity.java
@@ -42,6 +42,8 @@ public class AdidasActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AdidasActivity.this, NMDActivity.class);
+                int intValue = 1; // Replace this with your desired integer value
+                intent.putExtra("shoe", intValue);
                 startActivity(intent);
             }
         });
@@ -51,6 +53,8 @@ public class AdidasActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AdidasActivity.this, AlphaActivity.class);
+                int intValue = 1; // Replace this with your desired integer value
+                intent.putExtra("shoe", intValue);
                 startActivity(intent);
             }
         });
@@ -60,6 +64,8 @@ public class AdidasActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AdidasActivity.this, CloudActivity.class);
+                int intValue = 1; // Replace this with your desired integer value
+                intent.putExtra("shoe", intValue);
                 startActivity(intent);
             }
         });
@@ -68,6 +74,8 @@ public class AdidasActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AdidasActivity.this, MainActivity2.class);
+                int intValue = 1; // Replace this with your desired integer value
+                intent.putExtra("shoe", intValue);
                 startActivity(intent);
             }
         });
